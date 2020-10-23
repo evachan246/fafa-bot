4faa57f80eb3f1fe01ff5f3e1f4cdd5602ae274f
Eva <chingwa0812@gmail.com>
2020-10-23 19:27:29 +0800

    new

--- a.py
+++ a.py
@@ -76,12 +76,13 @@ def go(update, context):
         
 
     
-def image_handler(update, bot):    
+def image_handler(update, context):    
     update.message.reply_text("ABC")
     file = bot.getFile(update.message.photo[-1].file_id)
-    update.message.reply_text("file_id: " + str(update.message.photo.file_id))
+    context.bot.sendMessage(chat_id=update.message.chat.id,text = "file_id: " + str(update.message.photo.file_id))
     img2 = cv2.imread("img2.jpg",0)
     H2 = cv2.calcHist([img2], [0], None, [256], [0, 256])
+    context.bot.sendMessage(chat_id=update.message.chat.id,text = H2)
     update.message.reply_text(H2/H2)
 
 
