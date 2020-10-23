7172c4792c494e71dc63372f2ed8e6d4050b82ad
Eva <chingwa0812@gmail.com>
2020-10-23 19:30:36 +0800

    asda

--- a.py
+++ a.py
@@ -78,7 +78,6 @@ def go(update, context):
     
 def image_handler(update, context):    
     update.message.reply_text("ABC")
-    file = bot.getFile(update.message.photo[-1].file_id)
     context.bot.sendMessage(chat_id=update.message.chat.id,text = "file_id: " + str(update.message.photo.file_id))
     img2 = cv2.imread("img2.jpg",0)
     H2 = cv2.calcHist([img2], [0], None, [256], [0, 256])
