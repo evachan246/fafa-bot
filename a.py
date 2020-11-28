50f76b3bd66bb61de8613ea951c381e1be06a354
Eva <chingwa0812@gmail.com>
2020-11-28 10:36:29 +0800

    add

--- a.py
+++ a.py
@@ -90,8 +90,12 @@ def image_handler(update, context):
     img2 = cv2.imread("resource/img2.jpg",0)
     H2 = cv2.calcHist([img2], [0], None, [256], [0, 256])
     H2 = cv2.normalize(H2, H2, 0, 1, cv2.NORM_MINMAX, -1)
+    
+    img3 = cv2.imread("resource/img3.jpg",0)
+    H3 = cv2.calcHist([img3], [0], None, [256], [0, 256])
+    H3 = cv2.normalize(H3, H2, 0, 1, cv2.NORM_MINMAX, -1)
 
-    if(cv2.compareHist(photo, H1, 0)>=0.8 or cv2.compareHist(photo, H2, 0)>=0.8):
+    if(cv2.compareHist(photo, H1, 0)>=0.8 or cv2.compareHist(photo, H2, 0)>=0.8 or cv2.compareHist(photo, H3, 0)>=0.8):
         context.bot.deleteMessage(chat_id=update.message.chat.id, message_id=update.message.message_id)
         context.bot.kick_chat_member(chat_id=update.effective_chat.id, user_id = update.message.from_user.id)
 
